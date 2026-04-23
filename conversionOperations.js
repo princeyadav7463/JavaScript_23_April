44ad0062c3883f65e40fe0a48981d80c2eaa604e
princeyadav7463 <princeyadav875791@gmail.com>
2026-04-23 15:53:51 +0530

    ConversionOperations

---
+++ conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "3432Abc";
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1;
+//false => 0;
+//""=>false
+//"hitesh"=> true
+let isLoggedIn = "Prince";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
