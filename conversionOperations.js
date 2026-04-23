b65d4c6e76c1d4ec34331b3706f5a9ed0ebd8273
princeyadav7463 <princeyadav875791@gmail.com>
2026-04-23 18:04:02 +0530

    comparision

--- conversionOperations.js
+++ conversionOperations.js
@@ -20,3 +20,28 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 
 console.log(typeof stringNumber);
+
+// +++++++++++++++++++++Operation+++++++++++++++++++++
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3**4)
+// console.log(2%3);
+// console.log(3/4);
+
+let str1 = "hello";
+let str2 = "Prince";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 3);
+console.log(3+"3");
+console.log("1"+2+3);
+console.log(2+3+"4");
+
